89e09b2a3a64c3ef7f864bc8754e18ca9b2f5a04
Alex Bolduc <axbolduc@gmail.com>
2025-08-10 13:20:01 -0400

    add models and migration

--- packages/backend/jobs/models.py
+++ packages/backend/jobs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class JobStatus(models.Model):
+    STATUSES = {
+        "PENDING": "Pending",
+        "RUNNING": "Running",
+        "COMPLETED": "Completed",
+        "FAILED": "Failed",
+    }
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    status_type = models.CharField(max_length=32, choices=STATUSES)
+    timestamp = models.DateTimeField(auto_now_add=True)
